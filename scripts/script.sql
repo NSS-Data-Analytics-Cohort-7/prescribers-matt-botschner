c9d12cb7f127335e944bc3c71f522a584d4a145e
Matt Botschner <matt.botschner@gmail.com>
2022-08-11 21:16:53 -0500

    3 a ish

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,6 +1,6 @@
 -- Medicare Prescriptions Data In this exercise, you will be working with a database created from the 2017 Medicare Part D Prescriber Public Use File, available at https://data.cms.gov/provider-summary-by-type-of-service/medicare-part-d-prescribers/medicare-part-d-prescribers-by-provider-and-drug.
 
--- a. Which prescriber had the highest total number of claims (totaled over all drugs)?Report the npi and the total number of claims.
+--1) a. Which prescriber had the highest total number of claims (totaled over all drugs)?Report the npi and the total number of claims.
 --1154314755 @ 36 total claims
 SELECT npi, COUNT(total_claim_count) AS Total_Claims
 FROM prescription
@@ -16,13 +16,13 @@ GROUP BY prescription.npi, total_claim_count, prescriber.nppes_provider_first_na
 ORDER BY Total_Claims DESC;
 
 
--- a. Which specialty had the most total number of claims (totaled over all drugs)?
-SELECT prescription.npi, COUNT(total_claim_count) AS Total_Claims, prescriber.specialty_description
+--2) a. Which specialty had the most total number of claims (totaled over all drugs)?
+SELECT DISTINCT prescriber.specialty_description, SUM(total_claim_count) AS Total_Claims
 FROM prescription
 LEFT JOIN prescriber
 ON prescription.npi = prescriber.npi
-GROUP BY prescription.npi, total_claim_count, prescriber.specialty_description
-ORDER BY Total_Claims DESC;
+GROUP BY prescriber.specialty_description, total_claim_count
+ORDER BY prescriber.specialty_description DESC;
  
 -- b. Which specialty had the most total number of claims for opioids?
 
@@ -30,7 +30,7 @@ ORDER BY Total_Claims DESC;
 
 -- d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
 
--- a. Which drug (generic_name) had the highest total drug cost?
+--3) a. Which drug (generic_name) had the highest total drug cost?
 --Pirfednidone 
 SELECT prescription.drug_name, total_drug_cost, drug.generic_name
 FROM prescription
@@ -41,17 +41,17 @@ ORDER by total_drug_cost DESC;
 
 -- b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
 
--- a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
+--4) a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
 -- b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
--- a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
+--5) a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
 
 -- b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 
 -- c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 
--- a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+--6) a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
 
 -- b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
 
