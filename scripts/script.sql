d515b9450f6fd14b23f4260825e9b8430924c899
Matt Botschner <matt.botschner@gmail.com>
2022-08-13 10:18:54 -0500

    6A

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,10 +1,10 @@
 -- Medicare Prescriptions Data In this exercise, you will be working with a database created from the 2017 Medicare Part D Prescriber Public Use File, available at https://data.cms.gov/provider-summary-by-type-of-service/medicare-part-d-prescribers/medicare-part-d-prescribers-by-provider-and-drug.
 
 --1) a. Which prescriber had the highest total number of claims (totaled over all drugs)?Report the npi and the total number of claims.
---1154314755 @ 36 total claims
+--1356305197 @ 379 total claims
 SELECT npi, COUNT(total_claim_count) AS Total_Claims
 FROM prescription
-GROUP BY npi, total_claim_count
+GROUP BY npi
 ORDER BY Total_Claims DESC;
 -- b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, and the total number of claims.
 --JEFFREY LOWREY General Practice 36
@@ -16,16 +16,24 @@ GROUP BY prescription.npi, total_claim_count, prescriber.nppes_provider_first_na
 ORDER BY Total_Claims DESC;
 
 
---2) a. Which specialty had the most total number of claims (totaled over all drugs)?
+--2) a. Which specialty had the most total number of claims (totaled over all drugs)? Family Practice (9752347)
 SELECT DISTINCT prescriber.specialty_description, SUM(total_claim_count) AS Total_Claims
 FROM prescription
 LEFT JOIN prescriber
 ON prescription.npi = prescriber.npi
-GROUP BY prescriber.specialty_description, total_claim_count
-ORDER BY prescriber.specialty_description DESC;
+GROUP BY prescriber.specialty_description
+ORDER BY Total_Claims DESC;
  
--- b. Which specialty had the most total number of claims for opioids?
-
+-- b. Which specialty had the most total number of claims for opioids? Nurse Practioner
+SELECT DISTINCT prescriber.specialty_description, SUM(total_claim_count) AS Total_Claims
+FROM prescription
+LEFT JOIN prescriber
+ON prescription.npi = prescriber.npi
+LEFT JOIN drug 
+ON prescription.drug_name = drug.drug_name
+WHERE drug.opioid_drug_flag = 'Y'
+GROUP BY prescriber.specialty_description
+ORDER BY Total_Claims DESC;
 -- c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
 
 -- d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
@@ -43,13 +51,44 @@ ORDER by total_drug_cost DESC;
 
 --4) a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
+SELECT drug_name AS name, 
+    CASE WHEN opioid_drug_flag = 'Y' THEN 'Opioid'
+        WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+        ELSE 'neither' END drug_type
+FROM drug;
+
 -- b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+SELECT drug_name AS name, 
+    CASE WHEN opioid_drug_flag = 'Y' THEN 'Opioid'
+        WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+        ELSE 'neither' END drug_type
+FROM drug;
 
---5) a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
 
--- b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+--5) a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee. 33
+SELECT COUNT(cbsaname)
+FROM cbsa
+WHERE cbsaname LIKE '%TN';
 
--- c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+-- b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population. Largest = Nashville-Davidson-Murfreesboro-Franklin, TN Smallest = Morristown, TN
+SELECT c.cbsaname AS county, SUM(p.population) AS total_pop
+FROM cbsa AS c
+LEFT JOIN population AS p
+ON c.fipscounty = p.fipscounty
+WHERE p.population IS NOT NULL
+GROUP BY county
+ORDER BY total_pop DESC;
+
+-- c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population. SEVIER county, 95523
+SELECT f.county, p.population
+FROM population AS p
+LEFT JOIN fips_county AS f
+ON p.fipscounty = f.fipscounty
+LEFT JOIN cbsa AS c
+ON p.fipscounty = c.fipscounty
+WHERE c.fipscounty IS NULL
+ORDER BY p.population DESC;
+
 
 --6) a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
 
@@ -59,8 +98,32 @@ ORDER by total_drug_cost DESC;
 
 -- The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. Hint: The results from all 3 parts will have 637 rows.
 
--- a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management') in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). Warning: Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+--7) a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management') in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). Warning: Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
 
 -- b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
 
 -- c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
